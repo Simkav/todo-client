a057016d9d5a0db17191a1cca85c293d796b209e
Ivan Khomenko <Simkav2411@gmail.com>
2021-04-15 20:49:35 +0300

    Add api

---
+++ src/api/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const httpClient = axios.create({
+  baseURL: 'http://localhost:3000/api/todo',
+});
+
+export const getTask = id => {
+  return httpClient.get(`/${id}`);
+};
+
+export const getTasks = () => {
+  return httpClient.get(``);
+};
+
+export const createTask = data => {
+  return httpClient.post('', data);
+};
+
+export const updateTask = ({ data, id }) => {
+  return httpClient.patch(`/${id}`, data);
+};
+
+export const deleteTask = id => {
+  return httpClient.delete(`/${id}`);
+};
