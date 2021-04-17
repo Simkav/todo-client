d720678e00eb809d19a3d5d99c369d51d0bfb176
Ivan Khomenko <Simkav2411@gmail.com>
2021-04-17 22:21:29 +0300

    Add Sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,10 +1,12 @@
 import { takeLatest } from 'redux-saga/effects';
 import ACTION_TYPES from '../actions/actionTypes';
-import { createTaskSaga, getTasksSaga } from './taskSaga';
+import * as TaskSagas from './taskSaga';
 
 function * rootSaga () {
-  yield takeLatest(ACTION_TYPES.CREATE_TASK_REQUEST, createTaskSaga);
-  yield takeLatest(ACTION_TYPES.GET_TASKS_REQUEST, getTasksSaga);
+  yield takeLatest(ACTION_TYPES.CREATE_TASK_REQUEST, TaskSagas.createTaskSaga);
+  yield takeLatest(ACTION_TYPES.GET_TASKS_REQUEST, TaskSagas.getTasksSaga);
+  yield takeLatest(ACTION_TYPES.UPDATE_TASK_REQUEST, TaskSagas.updateTaskSaga);
+  yield takeLatest(ACTION_TYPES.DELETE_TASK_REQUEST, TaskSagas.deleteTaskSaga);
 }
 
 export default rootSaga;
--- src/sagas/taskSaga.js
+++ src/sagas/taskSaga.js
@@ -1,24 +1,56 @@
 import { put } from 'redux-saga/effects';
 import * as API from '../api';
 import * as TaskActionCreators from '../actions/taskCreators';
+//TODO ERROR HANDLING
+const parseError = error => {
+  const {
+    response: {
+      data: { error: errMessage },
+    },
+  } = error;
+  return errMessage;
+};
 
 export function * createTaskSaga (action) {
   try {
+    const { values } = action;
     const {
       data: { data },
-    } = yield API.createTask(action.values);
+    } = yield API.createTask(values);
     yield put(TaskActionCreators.createTaskSuccess(data));
   } catch (error) {
-    yield put(TaskActionCreators.createTaskError(error));
+    yield put(TaskActionCreators.createTaskError(parseError(error)));
   }
 }
-export function * getTasksSaga (action) {
+export function * getTasksSaga () {
   try {
     const {
       data: { data },
     } = yield API.getTasks();
     yield put(TaskActionCreators.getTasksSuccess(data));
   } catch (error) {
-    yield put(TaskActionCreators.getTasksError(error));
+    yield put(TaskActionCreators.getTasksError(parseError(error)));
+  }
+}
+export function * updateTaskSaga (action) {
+  try {
+    const { id, values } = action;
+    const {
+      data: { data },
+    } = yield API.updateTask({ id, values });
+    yield put(TaskActionCreators.updateTaskSuccess(data));
+  } catch (error) {
+    yield put(TaskActionCreators.updateTaskError(parseError(error)));
+  }
+}
+export function * deleteTaskSaga (action) {
+  try {
+    const { id } = action;
+    const {
+      data: { data },
+    } = yield API.deleteTask(id);
+    yield put(TaskActionCreators.deleteTaskSuccess(data));
+  } catch (error) {
+    yield put(TaskActionCreators.deleteTaskError(parseError(error)));
   }
 }
