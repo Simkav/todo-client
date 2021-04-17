b9ca50fcb79d004de0d3384b1486abe235c01c96
Ivan Khomenko <Simkav2411@gmail.com>
2021-04-17 22:21:52 +0300

    Complete TaskList component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,26 +3,57 @@ import { connect } from 'react-redux';
 import * as TaskActionCreators from '../actions/taskCreators';
 
 const TaskList = props => {
-  const { tasks, getTasksRequest } = props;
+  const {
+    tasks,
+    getTasksRequest,
+    updateTaskRequest,
+    deleteTaskRequest,
+    isFetching,
+    error,
+  } = props;
 
   const loadTasks = () => getTasksRequest();
   useEffect(() => {
     loadTasks();
-  });
+  }, []);
   return (
-    <ul>
-      {tasks.map(task => {
-        return (
-          <li key={task.id}>
-            <div>{task.body}</div>
-          </li>
-        );
-      })}
-    </ul>
+    <section>
+      {error && JSON.stringify(error)}
+      <div>TaskList</div>
+      {isFetching && 'Loading...'}
+      <ul>
+        {tasks.map(({ id, ...task }) => {
+          return (
+            <li key={id}>
+              <div>{task.body}</div>
+              <div>{task.deadline.slice(0, 10)}</div>
+              <input
+                type='checkbox'
+                checked={task.isDone}
+                onChange={({ target: { checked } }) => {
+                  updateTaskRequest({ id, values: { isDone: checked } });
+                }}
+              />
+              <button
+                onClick={() => {
+                  deleteTaskRequest(id);
+                }}
+              >
+                X
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 };
 
-const mapStateToProps = ({ task: { tasks } }) => ({ tasks });
+const mapStateToProps = ({ task: { tasks, error, isFetching } }) => ({
+  tasks,
+  error,
+  isFetching,
+});
 
 const mapDispatchToProps = dispatch => ({
   deleteTaskRequest: id => dispatch(TaskActionCreators.deleteTaskRequest(id)),
